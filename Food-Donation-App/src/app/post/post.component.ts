e0e61204ff218785d50e9c4ea873183036e601bd
AeriOS <srh627736@gmail.com>
2023-09-24 03:01:09 -0500

    post styles + thank you modal

--- Food-Donation-App/src/app/post/post.component.ts
+++ Food-Donation-App/src/app/post/post.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 
+declare var bootstrap: any;
+
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
+
+
 export class PostComponent {
+
+  showModal: boolean = false;
 
   constructor(private formBuilder:FormBuilder) {}
 
@@ -14,17 +20,29 @@ export class PostComponent {
     textForm: this.formBuilder.control(''),
     imageForm: this.formBuilder.control(''),
     vegetarianBox: this.formBuilder.control(''),
-    veganBox: this.formBuilder.control(''),
-    halalBox: this.formBuilder.control(''),
-    kosherBox: this.formBuilder.control('')
+    bbqBox: this.formBuilder.control(''),
+    deliBox: this.formBuilder.control(''),
+    bakeryBox: this.formBuilder.control('')
   })
 
   onSubmit() {
     console.log(this.postForm.controls['textForm'].value),
     console.log(this.postForm.controls['imageForm'].value),
     console.log(this.postForm.controls['vegetarianBox'].value),
-    console.log(this.postForm.controls['veganBox'].value),
-    console.log(this.postForm.controls['halalBox'].value),
-    console.log(this.postForm.controls['kosherBox'].value)
+    console.log(this.postForm.controls['bbqBox'].value),
+    console.log(this.postForm.controls['deliBox'].value),
+    console.log(this.postForm.controls['bakeryBox'].value)
+
+    // Open the modal
+    this.toggleModal();
+
+    // Prevent the form from doing a full page reload
+    return false;
   }
+
+  toggleModal() {
+    console.log("Toggling modal"); // Log to check if the function is called
+    this.showModal = !this.showModal;
+}
+
 }

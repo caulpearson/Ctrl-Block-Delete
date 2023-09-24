9523787fadab30ba1e72d531a0ba7d1ce68fe949
BrynSmith10 <120283357+BrynSmith10@users.noreply.github.com>
2023-09-24 07:44:40 -0500

    Adds thank you modal to contact page.

--- Food-Donation-App/src/app/contact/contact.component.ts
+++ Food-Donation-App/src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class ContactComponent {
 
+  showModal: boolean = false;
+
+  toggleModal() {
+    this.showModal = !this.showModal;
+  }
 }
